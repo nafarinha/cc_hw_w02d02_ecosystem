ea4c585ebde2fe4feba47ab8e0b90243d73cbeb0
Nuno Farinha <nadfarinha@gmail.com>
2019-01-22 21:34:17 +0000

    Create surrender fish method for river class

--- river.rb
+++ river.rb
@@ -1,11 +1,21 @@
 class River
-  attr_reader :name
+  attr_reader :name, :shoal
 
   def initialize(name)
     @name = name
+    @shoal = []
   end
 
+  def shoal_length()
+    return @shoal.length()
+  end
 
+  def add_fish(new_fish)
+    @shoal << new_fish
+  end
 
+  def surrender_fish
+    @shoal.pop()
+  end
 
 end
--- specs/river_spec.rb
+++ specs/river_spec.rb
@@ -1,10 +1,14 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../river")
+require_relative("../fish")
 
 class TestRiver < MiniTest::Test
 
   def setup
+
+    @fish_1 = Fish.new("pink salmon")
+    @fish_2 = Fish.new("atlantic salmon")
     @river = River.new("Amazon")
   end
 
@@ -12,5 +16,26 @@ class TestRiver < MiniTest::Test
     assert_equal("Amazon", @river.name())
   end
 
+  def test_can_create_shoal
+    assert_equal(Array, @river.shoal.class())
+  end
+
+  def test_add_fish
+    @river.add_fish(@fish_1)
+    assert_equal(1, @river.shoal_length())
+  end
+
+  def test_add_multiple_fishes
+    @river.add_fish(@fish_1)
+    @river.add_fish(@fish_2)
+    assert_equal(2, @river.shoal_length())
+  end
+
+  def test_surrender_fish
+    @river.add_fish(@fish_1)
+    @river.add_fish(@fish_2)
+    @river.surrender_fish()
+    assert_equal(1, @river.shoal_length())
+  end
 
 end
